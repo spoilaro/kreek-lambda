09b1ef754655e0d1ba3b9502bc8d98b255d8c569
spoilar <johannes.haapanen@gmail.com>
2022-01-09 15:18:20 +0200

    Search added

--- functions/data.js
+++ functions/data.js
@@ -1,8 +1,26 @@
 const axios = require("axios");
+const fs = require("fs");
 
 exports.handler = async (event, context, callback) => {
+  let place = event.queryStringParameters.place;
+  let place_data = require("../fact_cov19cov.dimensions.json");
+  place_data = place_data[0].children[0].children;
+
+  let place_sid = "";
+  let area_id = "";
+
+  place_data.forEach((elem) => {
+    console.log(elem.label);
+    if (elem.label == place) {
+      place_sid = elem.sid;
+      area_id = elem.id;
+    }
+  });
+
+  console.log(place_sid);
+
   let res = await axios.get(
-    "https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425#"
+    `https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425&filter=${area_id}-${place_sid}#`
   );
 
   let data = await res.data.dataset;
@@ -17,6 +35,7 @@ exports.handler = async (event, context, callback) => {
     statusCode: 200,
     headers,
     body: JSON.stringify(data),
+    //body: JSON.stringify(area_id),
   };
 
   return response;
