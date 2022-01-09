a2ebd7abf64712c5f5135f29266d745d52f4ca02
spoilar <johannes.haapanen@gmail.com>
2022-01-09 11:39:54 +0200

    Another update

--- functions/data.js
+++ functions/data.js
@@ -1,20 +1,16 @@
 const axios = require("axios");
 
 exports.handler = async (event, context, callback) => {
-  axios
-    .get(
-      "https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov.json"
-    )
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: res.data,
-      });
-    })
-    .catch((err) => {
-      callback({
-        statusCode: 400,
-        body: err,
-      });
-    });
+  let res = await axios.get(
+    "https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov.json"
+  );
+
+  let data = await res.data.dataset.label;
+
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+
+  return response;
 };
