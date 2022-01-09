daece3b2ca2b474774e5c5b4d9f30700915260e8
spoilar <johannes.haapanen@gmail.com>
2022-01-09 11:33:28 +0200

    Updated

--- functions/data.js
+++ functions/data.js
@@ -1,16 +1,17 @@
 const axios = require("axios");
 
-exports.handler = async (event) => {
-  //const res = await axios.get(
-  //"https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov.json"
-  //);
-
-  //const data = res.data;
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello World"),
-  };
-
-  return response;
+exports.handler = async (event, context, callback) => {
+  axios
+    .get(
+      "https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov.json"
+    )
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: res.data,
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
 };
