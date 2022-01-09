1269045f67571fb7ed04aa4fd0a0d2e838c46572
spoilar <johannes.haapanen@gmail.com>
2022-01-09 13:21:31 +0200

    Updated the link

--- functions/data.js
+++ functions/data.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.handler = async (event, context, callback) => {
   let res = await axios.get(
-    "https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov.json"
+    "https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425#"
   );
 
   let data = await res.data.dataset;
