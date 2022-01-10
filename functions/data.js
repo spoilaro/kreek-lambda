ed4a02dc022e670ed88b774adb38b45b3fbd5391
spoilar <johannes.haapanen@gmail.com>
2022-01-10 14:53:21 +0200

    Added pop_data

--- functions/data.js
+++ functions/data.js
@@ -8,6 +8,8 @@ exports.handler = async (event, context, callback) => {
 
   let place_sid = "";
   let area_id = "";
+  //let place_sid = "518362";
+  //let area_id = "area1";
 
   place_data.forEach((elem) => {
     console.log(elem.label);
@@ -23,7 +25,18 @@ exports.handler = async (event, context, callback) => {
     `https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425&filter=${area_id}-${place_sid}#`
   );
 
+  let pop_res = await axios.get(
+    `https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov;jsessionid=323860F3FC251F01590A16D47F235A97.apps5?row=cov_vac_dose-533170.&column=cov_vac_age-518413&filter=measure-433796&filter=${area_id}-${place_sid}`
+  );
+
+  let pop_data = await res.data.dataset;
+
   let data = await res.data.dataset;
+
+  const final_result = {
+    vaccine_data: data,
+    pop_data: pop_data,
+  };
 
   const headers = {
     "Access-Control-Allow-Origin": "*",
@@ -34,8 +47,8 @@ exports.handler = async (event, context, callback) => {
   let response = {
     statusCode: 200,
     headers,
-    body: JSON.stringify(data),
-    //body: JSON.stringify(area_id),
+    //body: JSON.stringify(data),
+    body: JSON.stringify(final_result),
   };
 
   return response;
