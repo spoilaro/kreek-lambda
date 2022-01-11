4ea33e12446f74c6b0a2d205f63299a6656d86a2
spoilar <johannes.haapanen@gmail.com>
2022-01-11 17:13:09 +0200

    Fixed place data

--- functions/data.js
+++ functions/data.js
@@ -19,17 +19,21 @@ exports.handler = async (event, context, callback) => {
     }
   });
 
-  console.log(place_sid);
-
   let res = await axios.get(
-    `https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425&filter=${area_id}-${place_sid}#`
+    //`https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425&filter=${area_id}-${place_sid}#`
+    `https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?row=cov_vac_dose-533170.&column=dateweek20201226-525425&filter=area1-518362#`
   );
 
   let pop_res = await axios.get(
-    `https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov;jsessionid=323860F3FC251F01590A16D47F235A97.apps5?row=cov_vac_dose-533170.&column=cov_vac_age-518413&filter=measure-433796&filter=${area_id}-${place_sid}`
+    //`https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov;jsessionid=323860F3FC251F01590A16D47F235A97.apps5?row=cov_vac_dose-533170.&column=cov_vac_age-518413&filter=measure-433796&filter=${area_id}-${place_sid}`
+    //`https://sampo.thl.fi/pivot/prod/en/vaccreg/cov19cov/fact_cov19cov;jsessionid=323860F3FC251F01590A16D47F235A97.apps5?row=cov_vac_dose-533170.&column=cov_vac_age-518413&filter=measure-433796&filter=area1-518362`
+    "https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json;jsessionid=323860F3FC251F01590A16D47F235A97.apps5?row=area-518362&column=cov_vac_age-518413.&filter=measure-433796&filter=area-518362"
   );
 
-  let pop_data = await res.data.dataset;
+  //let pop_data = await pop_res.data;
+  let pop_data = require("../pop_data.json");
+
+  console.log(pop_data);
 
   let data = await res.data.dataset;
 
